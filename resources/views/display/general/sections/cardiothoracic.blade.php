1517120a477b4f446b3253f1bdfa576cc98d5a8b
farhanfaridd96@gmail.com <farhanfaridd96@gmail.com>
2025-06-11 16:00:10 +0800

    update

--- resources/views/display/general/sections/cardiothoracic.blade.php
+++ resources/views/display/general/sections/cardiothoracic.blade.php
@@ -10,7 +10,7 @@
     <div class="row mt-3 px-2">
         <div class="mt-2" style="overflow-y: auto; flex: 1; padding: 0 10px;">
             <table class="table table-striped table-sm m-0" style="width: 100%; table-layout: fixed; font-size: 1.2rem; border: 1px solid #ffffff; border-collapse: collapse;">
-                <tbody style="font-size: 1.7rem; color:#000000; font-weight: 400;">
+                <tbody style="font-size: 1.8rem; color:#000000; font-weight: 600;">
                     <tr>
                         <td style="padding: 3px; width: 10%; border: 1px solid #ffffff;"><b>Consultant</b></td>
                         <td class="text-truncate" style="padding: 3px; border: 1px solid #ffffff;">{{ $rolesct['consultant'] }}</td>
